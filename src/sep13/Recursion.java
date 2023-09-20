4a2e420f9db2bd5a6ef7dae543e59d9b4e081b64
priyal.gupta <priyal.gupta@samarth.ac.in>
2023-09-20 13:07:47 +0530

    java-recursion-1

--- src/sep13/Recursion.java
+++ src/sep13/Recursion.java
@@ -1,7 +1,82 @@
 package sep13;
 
 public class Recursion {
-    public static void main(String[] args) {
-        
+    public static void printNumbers(int n) {
+        if(n == 6)
+            return;
+        System.out.println(n);
+        printNumbers(n+1);
     }
+
+    //Sum of first n natural numbers
+    public static void printSum(int i, int n, int sum) {
+        if(i == n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        printSum(i+1, n, sum);
+        System.out.println(i);
+
+    }
+
+    public static int printFactorial(int n){
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * printFactorial(n-1);
+    }
+
+    public static void printFibonnaci(int a, int b, int n){
+        if (n == 0)
+            return;
+        int c = a + b;
+        System.out.println(c);
+        printFibonnaci(b, c, n-1);
+    }
+
+    public static int xToPowOfN(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        if(x == 0)
+            return 0;
+        x *= xToPowOfN(x, n-1);
+        return x;
+    }
+
+    public static int calcPower(int x, int n){
+        if(n == 0)
+            return 1;
+        if(x == 0)
+            return 0;
+
+        //If n is even
+        if(n % 2 == 0){
+            return calcPower(x, n/2) * calcPower(x, n/2);
+        }else{
+            return calcPower(x, n/2) * calcPower(x, n/2) * x;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int n = 7;
+//        printNumbers(1);
+//        printSum(1, 10, 0);
+//        System.out.println(printFactorial(n));
+//        System.out.println(a);
+//        System.out.println(b);
+//        printFibonnaci(a, b, n - 2);
+//        System.out.println(xToPowOfN(0, 10));
+
+        // Question: Print x^n with stack height of logn
+        System.out.println(calcPower(2, 5));
+    }
+
+
+
 }
