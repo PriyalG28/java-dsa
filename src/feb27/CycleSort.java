2ac9d9c7a7d9bba1c693eca75b0579a0bbd438e9
PriyalG28 <aananya28tdn@gmail.com>
2024-03-18 16:50:56 +0530

    cyclic sort-1

--- src/feb27/CycleSort.java
+++ src/feb27/CycleSort.java
@@ -1,5 +1,6 @@
 package feb27;
 
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +11,10 @@ public class CycleSort {
     // Worst case example: O(N-1) ~ O(N)
 
     public static void main(String[] args) {
-        int[] arr = {3, 5, 2, 1};
+        int[] arr = {3,4,-1,1};
 //        cyclicSort(arr);
 
-        System.out.println(missingNum(arr));
+        System.out.println(firstMissingPositive(arr));
 
     }
 
@@ -70,12 +71,101 @@ public class CycleSort {
         }
         List<Integer> result = new ArrayList<>();
         for (int k = 0; k < nums.length; k++) {
-            if(nums[k] != k + 1){
+            if (nums[k] != k + 1) {
                 result.add(k + 1);
             }
         }
         return result;
 
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Ques.287 (LC) Find the duplicate number
+    public static int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (i != nums[i] - 1) {
+                if (nums[i] != nums[nums[i] - 1]) {
+                    swap(nums, i, nums[i] - 1);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return nums.length;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Ques.442 (LC) Find all duplicates in an array
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+        while (i < nums.length) {
+            int correctNum = nums[i] - 1;
+            if (i != correctNum && nums[i] != nums[correctNum]) {
+                swap(nums, i, correctNum);
+            } else {
+                i++;
+            }
+        }
+
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] != k + 1) {
+                result.add(nums[k]);
+            }
+        }
+        return result;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Ques.645 (LC) Set Mismatch
+    public static int[] findErrorNums(int[] nums) {
+        int[] result = new int[2];
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (i != correctIndex && nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] != k + 1) {
+                return new int[]{nums[k], k + 1};
+            }
+        }
+
+        return result;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Ques.41 (LC) First Missing Positive
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] != k + 1) {
+                return k + 1;
+            }
+        }
+
+        return nums.length + 1;
     }
 
 }
